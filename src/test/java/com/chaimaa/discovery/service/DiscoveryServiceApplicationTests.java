0a815cff673327cc23e140b397331ee564d6ebd0
ChaimaaRO <chaimaa_rouai@um5.ac.ma>
2023-08-25 08:00:59 +0100

    discovery-service

---
+++ src/test/java/com/chaimaa/discovery/service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaimaa.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
